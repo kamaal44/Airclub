cc89c88df3c35d67ca89106b2721c7319baf9134
Malgorzata Nelson <malgorzata.nelson@gmail.com>
2018-03-13 21:33:33 +0100

    added database connection, login and selects

--- project1.py
+++ project1.py
@@ -1,18 +1,25 @@
 import pymysql
 
+
 class DBConn:
+    
+    
+    
     def __init__(self):
         self.connString()
         print('Polaczenie ustanowione')
         #logowanie uzytkownika
-        access = self.login()
-        if(access == 1):
+        id = str(self.login())
+        perm = self.checkPerm(id)
+        pilotInstrID = self.welcome(perm, id)
+        print(pilotInstrID)
+        if(perm == 1):
             while(True):
-                dec = input('1. Select, 2. Delete, 3. Insert, 4. Quit')
+                dec = input('1. Select all, 2. Delete, 3. Insert, 4. Quit')
                 if(dec == '1'):
-                    self.select()
+                    self.selectAll()
                 elif(dec == '2'):
-                    self.select()
+                    self.selectAll()
                     self.delete()
                 elif(dec == '3'):
                     self.insert()
@@ -20,31 +27,60 @@ class DBConn:
                     self.connClose()
                 else:
                     print('Nieprawidłowy wybór')
-        elif(access == 0):
+        elif(perm == 0):
             while(True):
                 dec = input('1. Select, 2. Quit')
                 if(dec == '1'):
-                    self.select()
+                    self.select(pilotInstrID)
                 elif(dec == '2'):
                     self.connClose()
                 else:
                     print('Nieprawidłowy wybór')
         else:
             print('Błąd logowania')
-                
+                 
     def login(self):
         mail = input('podaj maila: ')
         passwd = input('podaj haslo: ')
-        self.c.execute('SELECT access FROM loginInfo WHERE email=%s AND password=%s', (mail, passwd))
+        self.c.execute('SELECT ID FROM loginInfo WHERE email=%s AND password=%s', (mail, passwd))
+        id = (self.c.fetchall()[0][0])
+        return id
+    
+    def checkPerm(self, id):
+        logID = id
+        self.c.execute('SELECT access FROM loginInfo WHERE ID=%s', (logID))
         try:
-            access = (self.c.fetchall()[0][0])
+            perm = int(self.c.fetchall()[0][0])
         except:
-            access = -1
-        return access
+            perm = -1
+        return perm     
+    
+    def welcome(self, perm, id):
+        logID = id
+        if (perm == 1):
+            self.c.execute('SELECT firstName, lastName FROM instructors WHERE loginID=%s', (logID))
+            for row in self.c.fetchall():
+                firstName = row[0]
+                lastName = row[1]
+            print("You are logged in as instructor %s %s. Welcome!" % (firstName, lastName))
+            self.c.execute('SELECT ID FROM instructors WHERE loginID=%s', (logID))
+            pilotInstrID = self.c.fetchall()[0][0]
+        else:
+            self.c.execute('SELECT firstName, lastName FROM pilots WHERE loginID=%s', (logID))
+            for row in self.c.fetchall():
+                firstName = row[0]
+                lastName = row[1]
+            print("You are logged in as pilot %s %s. Welcome!" % (firstName, lastName))
+            self.c.execute('SELECT ID FROM pilots WHERE loginID=%s', (logID))
+            pilotInstrID = self.c.fetchall()[0][0] 
+        return pilotInstrID
+
+    
     def connString(self):
         self.conn = pymysql.connect('localhost', 'testowy', 'password', 'airclub')
         self.c = self.conn.cursor()
-    def select(self):
+        
+    def selectAll(self):
         self.c.execute('SELECT * FROM vlogs;')
         #print(self.c.fetchall())
         for row in self.c.fetchall():
@@ -57,6 +93,13 @@ class DBConn:
             lvl = row[5]
             """
             print('%3s %15s %15s %12s %12s %11s %5s %5s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+            
+    def select(self, pilotInstrID):
+        id = pilotInstrID
+        self.c.execute('SELECT * FROM vlogs WHERE Pilot=(SELECT lastName from pilots WHERE ID=%s)', (id))
+        for row in self.c.fetchall():
+            print('%10s %6s %12s %12s %11s %5s %5s' % (row[1], row[2], row[3], row[4], row[5], row[6], row[7]))    
+            
     def delete(self):
         try:
             id = int(input('ID do usunięcia'))
@@ -66,6 +109,7 @@ class DBConn:
             self.select()
         except:
             print('Błędne ID')
+            
     def insert(self):
         try:
             name = input('podaj imie: ')
@@ -78,6 +122,7 @@ class DBConn:
             self.select()
         except:
             print('Podałeś niepoprawne dane')
+            
     def connClose(self):
         self.conn.close()
         
